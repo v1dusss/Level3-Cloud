0f2809595f2fd0b0b3e17f7600d7ec367d65796a
v1dusss <sivanathan.vidus@gmail.com>
2025-07-31 15:23:56 +0000

    feat: Add deploy script for OpenStack setup and image creation

---
+++ scripts/deploy-openstack.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Assumes local.conf already exists
+cd /opt/stack
+git clone https://opendev.org/openstack/devstack || true
+cd devstack
+
+./stack.sh
+
+source ~/devstack/openrc admin admin
+wget https://cloud-images.ubuntu.com/jammy/20250523/jammy-server-cloudimg-amd64.img
+
+openstack image create "Ubuntu 22.04 Jammy" \
+   --file jammy-server-cloudimg-amd64.img \
+   --disk-format qcow2 \
+   --container-format bare
+
+cd /opt/stack
+git clone https://github.com/v1dusss/Level3-Cloud.git Level3-Cloud
+cd Level3-Cloud
+# cd terraform
+# terraform init
+# terraform apply -auto-approve
